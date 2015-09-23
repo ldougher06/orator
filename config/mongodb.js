6cceeb27ceecd68b7937fc59979335182dceb5d8
logan dougher <logan.dougher@gmail.com>
2015-09-23 18:04:42 -0500

    .gitignore changes

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 var mongo = require('mongodb').MongoClient;
 //var mongoose = require('mongoose');
-var url = process.env.MONGODB_URL || 'mongodb://<ldougher>:<"Gandolfs_Kitchen394">@ds051953.mongolab.com:51953/orator';
+var url = process.env.MONGODB_URL;
 
 if(!global.db) {
   mongo.connect(url, function(err, db) {
