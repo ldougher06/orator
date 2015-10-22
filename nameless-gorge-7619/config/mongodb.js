145417e5c827dd30f91e172545d4f445211c62a6
logan dougher <logan.dougher@gmail.com>
2015-10-22 18:08:07 -0500

    changes to heroku config

--- nameless-gorge-7619/config/mongodb.js
+++ nameless-gorge-7619/config/mongodb.js
@@ -1,11 +1,17 @@
 var mongo = require('mongodb').MongoClient;
 //var mongoose = require('mongoose');
-var url = process.env.MONGODB_URL;
+var MONGODB_USER = process.env.MONGODB_USER;
+var MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+var url = process.env.MONGODB_URL ||
+  'mongodb://' +
+  MONGODB_USER +
+  ':' + MONGODB_PASSWORD +
+  '@ds051953.mongolab.com:51953/orator';
 
 if(!global.db) {
   mongo.connect(url, function(err, db) {
     console.log("mongo err: ", err);
+    console.log("connected to database: " + url);
     global.db = db;
   });
 };
-
