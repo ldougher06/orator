435368eb6a73c256a21ad49f36290408901f933d
logan dougher <logan.dougher@gmail.com>
2015-10-22 17:22:56 -0500

    testing mongo connection uri

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,10 +1,15 @@
 var mongo = require('mongodb').MongoClient;
 //var mongoose = require('mongoose');
-var url = process.env.MONGODB_URL;
+var url = process.env.MONGODB_URL ||
+  'mongodb://' +
+  process.env.MONGODB_USER +
+  ':' + process.env.MONGODB_PASSWORD +
+  '@ds051953.mongolab.com:51953/orator';
 
 if(!global.db) {
   mongo.connect(url, function(err, db) {
     console.log("mongo err: ", err);
+    console.log("connected to database: " + url);
     global.db = db;
   });
 };
